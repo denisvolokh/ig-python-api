803592a5957689b8b1279a0b0e5f29a0b1d6f3fb
Denis Volokh <denis.volokh@gmail.com>
2019-08-31 23:38:25 +0800

    added tests for client sentiment

--- ig/endpoints/clientsentiment.py
+++ ig/endpoints/clientsentiment.py
@@ -1,22 +1,6 @@
 from .apirequest import APIRequest
 
 
-class ClientMarketsSentiments(APIRequest):
-    """
-        Returns the client sentiment for the given instrument's market
-    """
-
-    ENDPOINT = "/clientsentiment"
-    EXTRA_HEADERS = {"VERSION": "1"}
-
-    def __init__(self, market_ids: list):
-        super(ClientMarketsSentiments, self).__init__(
-            endpoint=self.ENDPOINT, header=self.EXTRA_HEADERS
-        )
-
-        self.params = {"marketIds": ",".join(market_ids)}
-
-
 class ClientMarketSentiment(APIRequest):
     """
         Returns the client sentiment for the given instrument's market
